5ebd62c1fc865c62b9616b93362b47fe1a287023
Grace <145653394+EnigmaJoy@users.noreply.github.com>
2023-10-20 14:39:07 +0200

    Add files via upload

---
+++ Exercise2_lesson7.js
@@ -0,0 +1,19 @@
+/* 
+Esercizio 2: Lista della Spesa Interattiva
+- Crea una pagina HTML con un campo di input, un pulsante "Aggiungi" e un elenco (ul) vuoto.
+- Quando l'utente inserisce un elemento nella casella di input e fa clic sul pulsante "Aggiungi", l'elemento dovrebbe essere aggiunto come un nuovo elemento di elenco nella lista.
+*/
+
+
+/*function addToList() {
+    let food = document.getElementById("food").value;
+    let amount = document.getElementById("amount").value;
+    let unit = document.getElementById("unit").value;
+  
+    document.getElementById("foodlist").innerHTML +=
+      ('<li>'+food + ' ' + amount + ' ' + unit + '.</li>')
+  
+}; */
+
+// Just writing this code function in the script area inside Index.html
+// The background is modest but I'm going to improve it with some HTML-PUG animation
